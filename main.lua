51b1818a6fd6c30ee332e6de395fd89b55da3b14
aiden redmond <adnrdmnd@gmail.com>
2026-08-23 22:22:10 -0400

    display the x coordinate on screen

--- main.lua
+++ main.lua
@@ -1,6 +1,9 @@
 function love.load()
-	X = 100
+	FONT = love.graphics.getFont()
+	X = 0
+	DISPLAY_X = math.floor(X + 0.5)
 	WIDTH = love.graphics.getWidth()
+	HEIGHT = love.graphics.getHeight()
 	FORWARD = true
 end
 
@@ -12,12 +15,15 @@ function love.update(dt)
 	end
 
 	if FORWARD then
-		X = X + 100 * dt
+		X = X + 200 * dt
+		DISPLAY_X = math.floor(X + 0.5)
 	else
-		X = X - 100 * dt
+		X = X - 200 * dt
+		DISPLAY_X = math.floor(X + 0.5)
 	end
 end
 
 function love.draw()
 	love.graphics.rectangle("line", X, 75, 200, 450)
+	love.graphics.print(tostring(DISPLAY_X), (WIDTH / 2) - (FONT:getWidth(DISPLAY_X) / 2), 25)
 end
